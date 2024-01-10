0176fa73ccdc28630cba09e272333fdf2db3daa5
13155a1 <ppy040@gmail.com>
2024-01-10 21:54:32 +0900

    initial: first commit

---
+++ src/test/java/com/SpringSecurity/SignUpProject/SignUpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.SignUpProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
